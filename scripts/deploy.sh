1a784044f5ec98a5c898080bc0906829f3f838cf
phanhoangminhluan <phanhoangminhluan@gmail.com>
2022-04-29 16:42:15 +0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 mvn clean install -o
 
 mvn deploy:deploy-file \
--Durl=file:///Users/luan.phm/engineering/Projects/merlin/merlin-mapper/deployed-jar \
+-Durl=file://deployed-jar \
 -Dfile=./target/merlin-mapper-1.0-SNAPSHOT.jar \
 -DgroupId=com.merlin \
 -DartifactId=merlin-mapper \
